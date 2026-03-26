384088d53ed2cdad34b7411fc6c8514896568bc7
Hayden Zhou <mfkhao2009@outlook.com>
2026-03-26 16:12:47 +0800

    Add app icon and trayIcon

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QIcon>
+#include <QSystemTrayIcon>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    // Set Icon
+    QString logoPath = ":/assets/ShineFinch.png";
+    app.setWindowIcon(QIcon(logoPath));
+
+
+    // Set TrayIcon
+    QSystemTrayIcon trayIcon;
+    trayIcon.setIcon(QIcon(logoPath));
+    trayIcon.setToolTip("ShineFinch");
+    trayIcon.show();
+
+
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("ShineFinch", "Main");
+
+    return QCoreApplication::exec();
+}
